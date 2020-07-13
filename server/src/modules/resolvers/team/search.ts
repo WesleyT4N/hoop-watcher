0689e10600c4bc9a635a46b4af60e0a30b6c7c74
Wes T4N <wesleyt1998@gmail.com>
2020-07-13 17:27:25 -0400

    add graphql-codegen for generating ts types and update teams resolver typings

--- server/src/modules/resolvers/team/search.ts
+++ server/src/modules/resolvers/team/search.ts
@@ -5,7 +5,31 @@ import { Team } from "../../../types";
 import server from "../../../server";
 import { NBA_TEAM_COUNT } from "../../constants";
 
-const getTeams = async (): Promise<Array<object>> => {
+
+export type APITeam = {
+  id: string,
+  abbreviation: string,
+  city: string,
+  conference: string,
+  division: string,
+  full_name: string,
+  name: string,
+}
+
+export type DBTeam = {
+  id: string,
+  abbrev: string,
+  city: string,
+  conference: string,
+  division: string,
+  full_name: string,
+  name: string,
+  logo: string,
+  wins: number,
+  losses: number,
+}
+
+const getTeams = async (): Promise<Array<APITeam>> => {
   const response = await fetch("https://www.balldontlie.io/api/v1/teams");
   const data = await response.json();
   return data.data;
@@ -13,15 +37,16 @@ const getTeams = async (): Promise<Array<object>> => {
 
 const prepopulate = async (db: BetterSqlite3.Database): Promise<void> => {
   const teamData = await getTeams();
-  const teams = teamData.map((team: any) => ({
-    fullName: team.full_name,
+  const teams: Array<DBTeam> = teamData.map((team: APITeam) => ({
     abbrev: team.abbreviation,
     logo: `/assets/logos/${team.abbreviation}.svg`,
+    wins: 0,
+    losses: 0,
     ...team,
   }));
   const insert = db.prepare(`INSERT OR REPLACE INTO teams
     (id, name, full_name, abbrev, conference, division, logo)
-    VALUES (@id, @name, @fullName, @abbrev, @conference, @division, @logo)`);
+    VALUES (@id, @name, @full_name, @abbrev, @conference, @division, @logo)`);
 
   const insertMany = db.transaction(teams => {
     for (const team of teams) insert.run(team);
@@ -32,7 +57,7 @@ const prepopulate = async (db: BetterSqlite3.Database): Promise<void> => {
 
 const teamsInitialized = (db: BetterSqlite3.Database): boolean => {
   const stmt = db.prepare("SELECT COUNT(*) AS teamCount FROM teams");
-  const teamCount = stmt.get().teamCount;
+  const teamCount: number = stmt.get().teamCount;
   return teamCount === NBA_TEAM_COUNT;
 }
 
@@ -43,8 +68,9 @@ const resolver = (): Team[] => {
     prepopulate(db);
   }
   const stmt = db.prepare("SELECT * FROM teams");
-  return stmt.all().map((team: any): Team => ({
+  return stmt.all().map((team: DBTeam): Team => ({
     fullName: team.full_name,
+    games: [],
     ...team,
   }));
 };
--- server/src/types.ts
+++ server/src/types.ts
@@ -1,31 +1,259 @@
-export interface Team {
-  id: string;
-  name: string;
-  fullName: string;
-  abbrev: string;
-  conference: string;
-  division: string;
-  logo: string;
-  wins: number;
-  losses: number;
-  games: Array<Game>;
+import { GraphQLResolveInfo, GraphQLScalarType, GraphQLScalarTypeConfig } from 'graphql';
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: any }> = { [K in keyof T]: T[K] };
+export type RequireFields<T, K extends keyof T> = { [X in Exclude<keyof T, K>]?: T[X] } & { [P in K]-?: NonNullable<T[P]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  /** The `Upload` scalar type represents a file upload. */
+  Upload: any;
+};
+
+export enum CacheControlScope {
+  Public = 'PUBLIC',
+  Private = 'PRIVATE'
 }
 
-export interface Game {
-  id: string;
-  date: string;
-  home: string;
-  away: string;
-  season: number;
-  homeScore: number;
-  awayScore: number;
-  winner: string;
-  highlights: string;
+export type Game = {
+  __typename?: 'Game';
+  id: Scalars['ID'];
+  date: Scalars['String'];
+  home: Team;
+  away: Team;
+  season: Scalars['Int'];
+  homeScore: Scalars['Int'];
+  awayScore: Scalars['Int'];
+  winner: Team;
+  highlights: Scalars['String'];
+};
+
+export type GameList = {
+  __typename?: 'GameList';
+  items: Array<Maybe<Game>>;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  _empty?: Maybe<Scalars['String']>;
+  game?: Maybe<Game>;
+  team?: Maybe<Team>;
+  teams?: Maybe<Array<Maybe<Team>>>;
+  user?: Maybe<User>;
+};
+
+
+export type QueryGameArgs = {
+  id?: Maybe<Scalars['ID']>;
+};
+
+
+export type QueryTeamArgs = {
+  id?: Maybe<Scalars['ID']>;
+  name?: Maybe<Scalars['String']>;
+  abbrev?: Maybe<Scalars['String']>;
+};
+
+
+export type QueryUserArgs = {
+  id: Scalars['ID'];
+};
+
+export type Team = {
+  __typename?: 'Team';
+  id: Scalars['ID'];
+  name: Scalars['String'];
+  fullName: Scalars['String'];
+  abbrev: Scalars['String'];
+  conference: Scalars['String'];
+  division: Scalars['String'];
+  logo: Scalars['String'];
+  wins: Scalars['Int'];
+  losses: Scalars['Int'];
+  games: Array<Maybe<Game>>;
+};
+
+
+export type User = {
+  __typename?: 'User';
+  id: Scalars['ID'];
+  name: Scalars['String'];
+  email: Scalars['String'];
+  teams?: Maybe<Array<Maybe<Team>>>;
+};
+
+
+
+export type ResolverTypeWrapper<T> = Promise<T> | T;
+
+
+export type LegacyStitchingResolver<TResult, TParent, TContext, TArgs> = {
+  fragment: string;
+  resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
+};
+
+export type NewStitchingResolver<TResult, TParent, TContext, TArgs> = {
+  selectionSet: string;
+  resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
+};
+export type StitchingResolver<TResult, TParent, TContext, TArgs> = LegacyStitchingResolver<TResult, TParent, TContext, TArgs> | NewStitchingResolver<TResult, TParent, TContext, TArgs>;
+export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> =
+  | ResolverFn<TResult, TParent, TContext, TArgs>
+  | StitchingResolver<TResult, TParent, TContext, TArgs>;
+
+export type ResolverFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => Promise<TResult> | TResult;
+
+export type SubscriptionSubscribeFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => AsyncIterator<TResult> | Promise<AsyncIterator<TResult>>;
+
+export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => TResult | Promise<TResult>;
+
+export interface SubscriptionSubscriberObject<TResult, TKey extends string, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<{ [key in TKey]: TResult }, TParent, TContext, TArgs>;
+  resolve?: SubscriptionResolveFn<TResult, { [key in TKey]: TResult }, TContext, TArgs>;
 }
 
-export interface User {
-  id: string;
-  name: string;
-  email: string;
-  teams: Array<Team>;
+export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<any, TParent, TContext, TArgs>;
+  resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>;
 }
+
+export type SubscriptionObject<TResult, TKey extends string, TParent, TContext, TArgs> =
+  | SubscriptionSubscriberObject<TResult, TKey, TParent, TContext, TArgs>
+  | SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
+
+export type SubscriptionResolver<TResult, TKey extends string, TParent = {}, TContext = {}, TArgs = {}> =
+  | ((...args: any[]) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
+  | SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>;
+
+export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
+  parent: TParent,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => Maybe<TTypes> | Promise<Maybe<TTypes>>;
+
+export type IsTypeOfResolverFn<T = {}> = (obj: T, info: GraphQLResolveInfo) => boolean | Promise<boolean>;
+
+export type NextResolverFn<T> = () => Promise<T>;
+
+export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs = {}> = (
+  next: NextResolverFn<TResult>,
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => TResult | Promise<TResult>;
+
+/** Mapping between all available schema types and the resolvers types */
+export type ResolversTypes = {
+  Query: ResolverTypeWrapper<{}>;
+  String: ResolverTypeWrapper<Scalars['String']>;
+  ID: ResolverTypeWrapper<Scalars['ID']>;
+  Game: ResolverTypeWrapper<Game>;
+  Team: ResolverTypeWrapper<Team>;
+  Int: ResolverTypeWrapper<Scalars['Int']>;
+  User: ResolverTypeWrapper<User>;
+  Boolean: ResolverTypeWrapper<Scalars['Boolean']>;
+  GameList: ResolverTypeWrapper<GameList>;
+  CacheControlScope: CacheControlScope;
+  Upload: ResolverTypeWrapper<Scalars['Upload']>;
+};
+
+/** Mapping between all available schema types and the resolvers parents */
+export type ResolversParentTypes = {
+  Query: {};
+  String: Scalars['String'];
+  ID: Scalars['ID'];
+  Game: Game;
+  Team: Team;
+  Int: Scalars['Int'];
+  User: User;
+  Boolean: Scalars['Boolean'];
+  GameList: GameList;
+  Upload: Scalars['Upload'];
+};
+
+export type GameResolvers<ContextType = any, ParentType extends ResolversParentTypes['Game'] = ResolversParentTypes['Game']> = {
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  date?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  home?: Resolver<ResolversTypes['Team'], ParentType, ContextType>;
+  away?: Resolver<ResolversTypes['Team'], ParentType, ContextType>;
+  season?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  homeScore?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  awayScore?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  winner?: Resolver<ResolversTypes['Team'], ParentType, ContextType>;
+  highlights?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType>;
+};
+
+export type GameListResolvers<ContextType = any, ParentType extends ResolversParentTypes['GameList'] = ResolversParentTypes['GameList']> = {
+  items?: Resolver<Array<Maybe<ResolversTypes['Game']>>, ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType>;
+};
+
+export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = {
+  _empty?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  game?: Resolver<Maybe<ResolversTypes['Game']>, ParentType, ContextType, RequireFields<QueryGameArgs, 'id'>>;
+  team?: Resolver<Maybe<ResolversTypes['Team']>, ParentType, ContextType, RequireFields<QueryTeamArgs, 'id' | 'name' | 'abbrev'>>;
+  teams?: Resolver<Maybe<Array<Maybe<ResolversTypes['Team']>>>, ParentType, ContextType>;
+  user?: Resolver<Maybe<ResolversTypes['User']>, ParentType, ContextType, RequireFields<QueryUserArgs, 'id'>>;
+};
+
+export type TeamResolvers<ContextType = any, ParentType extends ResolversParentTypes['Team'] = ResolversParentTypes['Team']> = {
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  fullName?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  abbrev?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  conference?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  division?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  logo?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  wins?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  losses?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  games?: Resolver<Array<Maybe<ResolversTypes['Game']>>, ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType>;
+};
+
+export interface UploadScalarConfig extends GraphQLScalarTypeConfig<ResolversTypes['Upload'], any> {
+  name: 'Upload';
+}
+
+export type UserResolvers<ContextType = any, ParentType extends ResolversParentTypes['User'] = ResolversParentTypes['User']> = {
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  email?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  teams?: Resolver<Maybe<Array<Maybe<ResolversTypes['Team']>>>, ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType>;
+};
+
+export type Resolvers<ContextType = any> = {
+  Game?: GameResolvers<ContextType>;
+  GameList?: GameListResolvers<ContextType>;
+  Query?: QueryResolvers<ContextType>;
+  Team?: TeamResolvers<ContextType>;
+  Upload?: GraphQLScalarType;
+  User?: UserResolvers<ContextType>;
+};
+
+
+/**
+ * @deprecated
+ * Use "Resolvers" root object instead. If you wish to get "IResolvers", add "typesPrefix: I" to your config.
+ */
+export type IResolvers<ContextType = any> = Resolvers<ContextType>;
