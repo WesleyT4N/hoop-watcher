ad05adee98bc1653bd3c3e56a6de6fb732b2aab6
Wes T4N <wesleyt1998@gmail.com>
2020-07-17 15:42:48 -0400

    finish team retrieve resolver unit tests

--- server/src/modules/resolvers/team/__tests__/retrieve.test.ts
+++ server/src/modules/resolvers/team/__tests__/retrieve.test.ts
@@ -1,3 +1,6 @@
+import fetchMock from "jest-fetch-mock";
+fetchMock.enableMocks();
+
 import { mocked } from "ts-jest/utils";
 import { ApolloError } from "apollo-server-express";
 
@@ -32,13 +35,67 @@ export const mockDb = {
       id === mockTeam.id || name === mockTeam.name || abbrev === mockTeam.abbrev
         ? mockTeam
         : null,
+    run: (_) => jest.fn(),
   }),
+  transaction: (_) => jest.fn(),
 };
 mockedServer.getDb.mockReturnValue(mockDb);
 
+const mockAPIResponse = {
+  data: [
+    {
+      id: "47179",
+      date: "2019-01-30T00:00:00.000Z",
+      home_team: {
+        id: "1",
+        name: "mock name",
+        abbreviation: "MCK",
+        city: "mock",
+        conference: "mock",
+        division: "mock",
+        full_name: "mock full name",
+      },
+      home_team_score: 126,
+      period: 4,
+      postseason: false,
+      season: 2018,
+      status: "Final",
+      time: " ",
+      visitor_team: {
+        id: "4",
+        abbreviation: "CHA",
+        city: "Charlotte",
+        conference: "East",
+        division: "Southeast",
+        full_name: "Charlotte Hornets",
+        name: "Hornets",
+      },
+      visitor_team_score: 94,
+    },
+  ],
+};
+
+const expectRetrievedTeamMatchesExpected = async (retrieveByArgs, expected) => {
+  expect.assertions(1);
+  const result = await retrieve({}, retrieveByArgs);
+  expect(result).toEqual(expected);
+};
+
+const expectApolloErrorThrown = async () => {
+  expect.assertions(1);
+  try {
+    await retrieve({}, { id: "some-other-id", name: "", abbrev: "" });
+  } catch (e) {
+    expect(e).toBeInstanceOf(ApolloError);
+  }
+};
+
 describe("team retrieve resolver", () => {
   describe("given no need to update team", () => {
-    mockedUtil.shouldUpdate.mockReturnValue(false);
+    beforeAll(() => {
+      mockedUtil.shouldUpdate.mockReturnValue(false);
+      fetchMock.resetMocks();
+    });
 
     describe("given the team exists", () => {
       const expectedTeam: Team = {
@@ -46,43 +103,68 @@ describe("team retrieve resolver", () => {
         fullName: mockTeam.full_name,
       };
 
-      it("can retrieve by team id", async () => {
-        expect.assertions(1);
-        const result = await retrieve(
-          {},
-          { id: mockTeam.id, name: "", abbrev: "" }
-        );
-        expect(result).toEqual(expectedTeam);
-      });
+      it("can retrieve by team id", () =>
+        expectRetrievedTeamMatchesExpected(
+          { id: mockTeam.id, name: "", abbrev: "" },
+          expectedTeam
+        ));
 
-      it("can retrieve by team name", async () => {
-        expect.assertions(1);
-        const result = await retrieve(
-          {},
-          { id: "", name: mockTeam.name, abbrev: "" }
-        );
-        expect(result).toEqual(expectedTeam);
-      });
+      it("can retrieve by team name", () =>
+        expectRetrievedTeamMatchesExpected(
+          { id: "", name: mockTeam.name, abbrev: "" },
+          expectedTeam
+        ));
 
-      it("can retrieve by team abbreviation", async () => {
-        expect.assertions(1);
-        const result = await retrieve(
-          {},
-          { id: "", name: "", abbrev: mockTeam.abbrev }
-        );
-        expect(result).toEqual(expectedTeam);
-      });
+      it("can retrieve by team abbreviation", () =>
+        expectRetrievedTeamMatchesExpected(
+          { id: "", name: "", abbrev: mockTeam.abbrev },
+          expectedTeam
+        ));
     });
 
     describe("given the team does not exist", () => {
-      it("throws ApolloError", async () => {
-        expect.assertions(1);
-        try {
-          await retrieve({}, { id: "some-other-id", name: "", abbrev: "" });
-        } catch (e) {
-          expect(e).toBeInstanceOf(ApolloError);
-        }
-      });
+      it("throws ApolloError", expectApolloErrorThrown);
+    });
+  });
+
+  describe("given need to update team", () => {
+    beforeAll(() => {
+      mockedUtil.shouldUpdate.mockReturnValue(true);
+      mockedUtil.getCurrSeasonYear.mockReturnValue(2020);
+      fetchMock.resetMocks();
+      fetchMock.doMock();
+      fetchMock.mockResponse(JSON.stringify(mockAPIResponse));
+    });
+
+    describe("given the team exists", () => {
+      const expectedTeam: Team = {
+        ...mockTeam,
+        fullName: mockTeam.full_name,
+        wins: 1,
+        losses: 0,
+      };
+
+      it("can retrieve by team id", () =>
+        expectRetrievedTeamMatchesExpected(
+          { id: mockTeam.id, name: "", abbrev: "" },
+          expectedTeam
+        ));
+
+      it("can retrieve by team name", () =>
+        expectRetrievedTeamMatchesExpected(
+          { id: "", name: mockTeam.name, abbrev: "" },
+          expectedTeam
+        ));
+
+      it("can retrieve by team abbreviation", () =>
+        expectRetrievedTeamMatchesExpected(
+          { id: "", name: "", abbrev: mockTeam.abbrev },
+          expectedTeam
+        ));
+    });
+
+    describe("given the team does not exist", () => {
+      it("throws ApolloError", expectApolloErrorThrown);
     });
   });
 });
